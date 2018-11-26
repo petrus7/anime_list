11142d7fb626569a81ada6ee9365dee1ff0afee4
Piotr Dankowski <piotr.dankowski@autea.eu>
2018-11-26 13:36:12 +0100

    add app entrypoint

---
+++ run.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    print('ANIME LIST LIST')
+    print('ANIME LIST LIST')
+    print('ANIME LIST LIST')
+    print('ANIME LIST LIST')
+    print('ANIME LIST LIST')
+    print('ANIME LIST LIST')
+    print('ANIME LIST LIST')
